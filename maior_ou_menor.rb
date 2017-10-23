15a49d243eb65486fbadffd6bfb905b04ee7bf29
Lucas Vaz Tassoni <lucasvt2@gmail.com>
2017-10-23 00:39:38 -0200

    Iniciando com Ruby

---
+++ maior_ou_menor.rb
@@ -0,0 +1,45 @@
+puts "Bem vindo ao jogo de adivinhação"
+puts "Qual é seu nome"
+nome = gets
+
+puts
+puts
+puts
+puts
+puts "Começamos o jogo para você " + nome
+
+puts "Escolhendo um numero secreto entre 0 e 200"
+numero_secreto == 175
+puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+
+puts
+puts
+puts
+puts
+puts "tentativa 1"
+
+puts "entre com seu numero"
+chute = gets
+puts "sera que acertou? Você chutou " + chute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
